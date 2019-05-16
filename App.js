83fda5f3dfd6761b0333dc0454ab76bb372780d6
Joao Lopes <joaopclopes@gmail.com>
2019-05-16 21:14:14 +0100

    Add assets; clean up API call

--- App.js
+++ App.js
@@ -16,31 +16,32 @@ export default class App extends React.Component {
   }
 
   reloadData = () => {
+    var busStopId = 80
+
     const url =
       'http://www.horariosdofunchal.pt/index.php?option=com_simip&task=consultaParagem&id=';
     fetch(url, {
       method: 'POST',
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
-        Accept: '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        Referer:
-          'http://www.horariosdofunchal.pt/index.php?option=com_simip&task=consultaParagem&id=',
-        'If-Modified-Since': 'Sat, 1 Jan 2000 00:00:00 GMT',
-        'content-type': 'application/x-www-form-urlencoded',
-        DNT: '1',
-        Connection: 'keep-alive',
-        Pragma: 'no-cache',
-        'Cache-Control': 'no-cache',
-      },
-      body: "xjxfun=fazPedido&xjxargs[]=<xjxobj><e><k>carreirasurb</k><v>13</v></e><e><k>carreirasinter</k><v>0</v></e><e><k>sentidos</k><v>17862</v></e><e><k>paragens</k><v>80</v></e><e><k>pesquisa</k><v>Pesquisar</v></e></xjxobj>",
+      body: [
+        `xjxfun=fazPedido`,
+        `xjxargs[]=<xjxobj><e><k>paragens</k><v>${busStopId}</v></e></xjxobj>`
+      ].join('&'),
     })
       .then(data => data.text())
       .then(data => {
-        var xmlData = new DOMParser().parseFromString(data)
+        try {
+          var xmlData = new DOMParser().parseFromString(data)
+        } catch (error) {
+          console.error(error)
+          console.error(data)
+        }
         var textPayload = xmlData.childNodes[1].childNodes[1].childNodes[0].nodeValue
-        var htmlData = new DOMParser().parseFromString(textPayload)
+        try {
+          var htmlData = new DOMParser().parseFromString(textPayload)
+        } catch (error) {
+          console.error(error)
+          console.error(textPayload)
+        }
 
         var container = htmlData.getElementById('infoParagens')
         var title = htmlData.getElementById('nomeparagem').textContent
@@ -76,7 +77,7 @@ export default class App extends React.Component {
   renderBusData = () => {
     return Object.keys(this.state.busData).map(bus => {
       return (
-        <View>
+        <View key={bus}>
           <View style={styles.busLabel}><Text style={styles.bus}>{bus} {this.getBusName(bus)}: </Text></View>
           <Text style={styles.minutos}>{this.state.busData[bus].join(', ')} min.</Text>
         </View> 
